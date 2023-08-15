02c80afd6c751886b897e41d51088baee78b73e7
Raviraj <ravirajchilka10@gmail.com>
2023-08-14 23:45:08 -0400

    Create find_mean_mode_modal.py

---
+++ Python_Programs/find_mean_mode_modal.py
@@ -0,0 +1,41 @@
+ 
+# find mean,mode,modal for 10 random numbers
+
+import random
+
+class Stats:
+    def __init__(self,nums_array,sum,number_of_values):
+        self.sum = sum
+        self.nums_array = nums_array
+        self.number_of_values = number_of_values 
+
+    def find_mean(self):
+        for i in range(self.number_of_values):
+            self.nums_array.append(random.randint(1,5))
+            self.sum += self.nums_array[i]
+        print("Generated list:\n",self.nums_array)
+        return round(self.sum/self.number_of_values,1)
+
+    def find_mode(self):
+        modes = []
+        for value in self.nums_array:
+            if self.nums_array.count(value) > 1:
+                if (value in modes) == False:
+                    modes.append(value)
+        return modes
+    
+
+
+stat = Stats([],0,7)
+print("Mean: ",stat.find_mean())
+print("repeated",stat.find_mode())
+
+
+
+
+
+
+
+
+
+
